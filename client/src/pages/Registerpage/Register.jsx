839c11c2d6ff0c7c02f684678f631c20f4cb502f
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2020-02-11 22:00:51 +0530

    Register Page Layout

--- client/src/pages/Registerpage/Register.jsx
+++ client/src/pages/Registerpage/Register.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-
+import "./register.css";
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="register-page mt-5">
+      <p className="register-page-head">Register</p>
+      <div className="register-page-form">
+        <label className="register-page-label" for="name">
+          Full Name
+        </label>
+        <input type="text" id="name" className="register-form-input" />
+        <label className="register-page-label" for="email">
+          Email
+        </label>
+        <input type="email" id="email" className="register-form-input" />
+        <label className="register-page-label" for="password">
+          Password
+        </label>
+        <input type="password" id="password" className="register-form-input" />
+        <label className="register-page-label" for="confirm">
+          Confirm Password
+        </label>
+        <input type="password" id="confirm" className="register-form-input" />
+      </div>
     </div>
   );
 };
