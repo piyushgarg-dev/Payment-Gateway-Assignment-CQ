08651a5401557482b1e492aed17c28b06f36c584
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2020-02-11 23:15:50 +0530

    Profile Page

--- client/src/pages/Profilepage/Profile.jsx
+++ client/src/pages/Profilepage/Profile.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "./profile.css";
 const Profile = ({ user }) => {
   console.log(user);
   return (
     <div className="profile-page">
-      <h1>Profile Page</h1>
+      <nav class="profile-page-nav">Hello {user.displayName}</nav>
+      <div className="row credits">
+        <div className="col-md-6 col-sm-12 col-lg-6 bg-primary p-2 text-white">
+          Active Credits: {user.activeCredits}
+        </div>
+        <div className="col-md-6 col-sm-12 col-lg-6 bg-warning p-2 text-white">
+          Pending Credits: {user.pendingCredits}
+        </div>
+      </div>
     </div>
   );
 };
